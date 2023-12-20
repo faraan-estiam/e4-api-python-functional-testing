ab33a4cd271918f1e3509b0b38a64bb6fe0b6376
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-12-20 16:59:52 +0100

    modify return type

--- classes/RestaurantReviews.py
+++ classes/RestaurantReviews.py
@@ -5,8 +5,9 @@ class RestaurantReviews:
     def add_review(self, restaurant, comment, rating) :
         if rating<0 or rating>5 : raise ValueError(f"rating must be between 0 and 5 (given: {rating})")
         if restaurant in self.reviews.keys()  : raise ReviewAlreadyExists(restaurant+" already has a review")
-        self.reviews[restaurant]={"comment":comment, "rating":rating}
-        return self
+        review={"comment":comment, "rating":rating}
+        self.reviews[restaurant]=review
+        return review
     
     def get_review(self, restaurant) :
         if not restaurant in self.reviews.keys() : raise ReviewDoesNotExists(restaurant + " does not have review")
@@ -15,9 +16,14 @@ class RestaurantReviews:
     def update_review(self, restaurant, comment, rating) :
         if not restaurant in self.reviews.keys() : raise ReviewDoesNotExists(restaurant + " does not have review")
         if rating<0 or rating>5 : raise ValueError(f"rating must be between 0 and 5 (given: {rating})")
-        self.reviews[restaurant]={"comment":comment, "rating":rating}
-        return self
+        review={"comment":comment, "rating":rating}
+        self.reviews[restaurant]=review
+        return review
     
+    def remove_review(self, restaurant):
+        if not restaurant in self.reviews.keys() : raise ReviewDoesNotExists(restaurant + " does not have review")
+        return self.reviews.pop(restaurant)
+
 class ReviewAlreadyExists (Exception) :
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
