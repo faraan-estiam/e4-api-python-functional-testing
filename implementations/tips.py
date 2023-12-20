f5f03959530184f4a32bbd3c56de31b1a8e3e8d4
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-12-20 13:33:08 +0100

    added error handling

--- implementations/tips.py
+++ implementations/tips.py
@@ -1,4 +1,11 @@
+class NegativeValueError (Exception):
+    pass
+
 def total_with_tips (bill, percentage) :
+    if bill < 0 :
+        raise NegativeValueError("bill should be positive")
+    if percentage < 0 :
+        raise NegativeValueError("percentage should be positive")
     tip = bill*percentage/100
     tip = min(tip, 500)
     total = bill + tip 
--- tests/test_tips.py
+++ tests/test_tips.py
@@ -1,16 +1,27 @@
-from implementations.tips import total_with_tips
+from implementations.tips import total_with_tips, NegativeValueError
+import pytest
 
-def test_100_20 () :
-    assert total_with_tips(100,20) == 120
 
-def test_100_notip () :
+def test_poor_service () :
     assert total_with_tips(100,0) == 100
 
 def test_cheap_bill () :
-    assert total_with_tips(1,20) == 5
+    assert total_with_tips(2.30,20) == 5
+
+def test_normal_bill () :
+    assert total_with_tips(100,20) == 120
 
 def test_expensive_bill () :
     assert total_with_tips(5000,15) == 5500
 
 def test_cents () : 
     assert total_with_tips(10,33.3333) == 13.33
+
+def test_negative_error () :
+    with pytest.raises(NegativeValueError) as exceptionBill :
+        total_with_tips(-4,20)
+    assert str(exceptionBill.value) == "bill should be positive"
+
+    with pytest.raises(NegativeValueError) as exceptionTip :
+        total_with_tips(4,-20)
+    assert str(exceptionTip.value) == "percentage should be positive"
